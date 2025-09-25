9f6ff32e054f88a32d7a33d65e22899850701012
codey298 <dothiphuc@MacBook-Pro-cua-Do.local>
2025-09-25 17:40:39 +0700

    update-docker

--- pkg/payment/config/config_deposit.go
+++ pkg/payment/config/config_deposit.go
@@ -5,7 +5,7 @@ import "money-transfer-demo/pkg/util/env"
 const (
 	defaultDepositClientIP = "AW2SoYSo5-oYeHkLYG2mMDo_JBkth-bX0W3k3ZNXAm96Pz4c10qKFVzsLn5F2AjpYURYI2Y0Mjp9NUOq"
 	defaultDepositSecret   = "EJGdh7O1GF-eVc8N5qgu06yAOzBXCwJ51lPQrXOrTUDk7H1CBLals0UXEMlLhHcaR8UH1oyMDC6zKUyW"
-	defaultReturnURL       = "http://localhost:5089/api/mem/deposit"
+	defaultReturnURL       = "http://localhost:5090/api/mem/deposit"
 )
 
 type DepositConfig struct {
